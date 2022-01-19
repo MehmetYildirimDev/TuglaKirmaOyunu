f0b73a6fcadf256811bb4fed79de1883c5e58670
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-01-19 21:01:38 +0300

    update

---
+++ Tugla Kirma/Assets/Scripts/OyunaGecis.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OyunaGecis : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void SahneGecis()
+    {
+        SceneManager.LoadScene("Oyun1");
+    }
+}
